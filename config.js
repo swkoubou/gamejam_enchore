3d4e42fed97daa23c05a17b509559859f08f2c64
uryoya <urano.works.mail@gmail.com>
2016-02-11 22:38:40 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,104 @@
+/* 座標 */
+WIDTH = 1100;
+HEIGHT = 1400;
+
+/* 画像 */
+REACTINO_BIG_HART_PINK = "./img/ui/big_hart_pink.png";
+REACTINO_MINI_HART_PINK = "./img/ui/mini_hart_pink.png";
+
+MAIN_GIRL = "./img/girl/completeWhiteonepice.png";
+MAIN_GIRL_SUMMER = "./img/girl/completeSummerclothes.png";
+MAIN_GIRL_WINTER = "./img/girl/completeWinterclothes.png";
+MAIN_GIRL_KIMONO_PURPLE = "./img/girl/kimono_purple.png";
+MAIN_GIRL_KIMONO_GRAY = "./img/girl/kimono_gray.png";
+MAIN_GIRL_KIMONO_RED = "./img/girl/kimono_red.png";
+MAIN_GIRL_MAID_BLUE = "./img/girl/maid_blue.png";
+MAIN_GIRL_MAID_GREEN = "./img/girl/maid_green.png";
+MAIN_GIRL_MAID_PINK = "./img/girl/maid_pink.png";
+MAIN_GIRL_OLD_SWIMSUIT = "./img/girl/completeoldswimsuit.png";
+MAIN_GIRL_OLD_SWIMSUIT_R18 = "./img/girl/R18.png";
+
+DEFAULT_ONEPIECE = "./img/ui/onpiece.png";
+NORMAL_SUMMER_UNIFORM = "./img/ui/natuhuku.png";
+NORMAL_WINTER_UNIFORM = "./img/ui/huyuhuku.png";
+KIMONO_PURPLE = "./img/ui/kimono.png";
+KIMONO_GRAY = "./img/ui/kimono-\ 2.png";
+KIMONO_RED = "./img/ui/kimono-\ 3\ .png";
+MAID_BLUE = "./img/ui/meid.png";
+MAID_GREEN = "./img/ui/meid\ -\ 2.png";
+MAID_PINK = "./img/ui/meid\ -\ 3.png";
+SUPERRARE_OLD_SWIMSUIT = "./img/ui/sukumizu.png";
+OLD_SWIMSIT_R18 = "./img/ui/er18.png";
+
+HART_GAUGE_0 = "./img/ui/hart_par_0.png";
+HART_GAUGE_30 = "./img/ui/hart_par_30.png";
+HART_GAUGE_60 = "./img/ui/hart_par_60.png";
+HART_GAUGE_100 = "./img/ui/hart_par_100.png";
+SIDE_BAR = "./img/ui/sidebar.png";
+
+GATYA = "./img/ui/gatya.png";
+
+GATHA_FIRST = "./img/ui/gamejam_gacha_first.png";
+GATHA_SPLIGHT = "./img/ui/gamejam_gacha_turn_splight.png";
+GATHA_HART = "./img/ui/gamejam_gacha_hart_pink.png";
+
+PRELOAD = [
+    REACTINO_BIG_HART_PINK,
+    REACTINO_MINI_HART_PINK,
+    MAIN_GIRL,
+    MAIN_GIRL_SUMMER,
+    MAIN_GIRL_WINTER,
+    MAIN_GIRL_KIMONO_PURPLE,
+    MAIN_GIRL_KIMONO_GRAY,
+    MAIN_GIRL_KIMONO_RED,
+    MAIN_GIRL_MAID_BLUE,
+    MAIN_GIRL_MAID_GREEN,
+    MAIN_GIRL_MAID_PINK,
+    MAIN_GIRL_OLD_SWIMSUIT,
+    MAIN_GIRL_OLD_SWIMSUIT_R18 ,
+    DEFAULT_ONEPIECE ,
+    NORMAL_SUMMER_UNIFORM ,
+    NORMAL_WINTER_UNIFORM,
+    KIMONO_PURPLE ,
+    KIMONO_GRAY,
+    KIMONO_RED,
+    MAID_BLUE,
+    MAID_GREEN ,
+    MAID_PINK,
+    SUPERRARE_OLD_SWIMSUIT,
+    OLD_SWIMSIT_R18,
+    HART_GAUGE_0,
+    HART_GAUGE_30,
+    HART_GAUGE_60,
+    HART_GAUGE_100,
+    SIDE_BAR ,
+    GATYA ,
+    GATHA_FIRST,
+    GATHA_SPLIGHT,
+    GATHA_HART
+];
+
+/*
+PRELOAD = [REACTINO_BIG_HART_PINK, REACTINO_MINI_HART_PINK, MAIN_GIRL
+    , HART_GAUGE_0, HART_GAUGE_30, HART_GAUGE_60, HART_GAUGE_100, SIDE_BAR
+    , NORMAL_SUMMER_UNIFORM, NORMAL_WINTER_UNIFORM, RARE_KIMONO, SUPERRARE_OLD_SWIMSUIT, GATYA
+    , MAIN_GIRL_WINTER, MAIN_GIRL_WHITE_ONEPIECE, MAIN_GIRL_KIMONO_PURPLE, MAIN_GIRL_OLD_SWIMSUIT
+    , GATHA_FIRST, GATHA_HART, GATHA_SPLIGHT];
+    */
+
+LOVE_POINT = 0;
+
+COSTUME_FLAGS = {
+    defaultonepiece: true,
+    normalsummer: false,
+    normalwinter: false,
+    kimono_purple: false,
+    kimono_gray: false,
+    kimono_red: false,
+    maid_blue: false,
+    maid_green: false,
+    maid_pink: false,
+    oldswimsuit: false,
+    oldswimsuit_r18: false
+};
+
